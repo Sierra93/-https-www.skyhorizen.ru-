e2c730def1cf0314e012e505f1db3a11ba682887
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2019-07-05 00:28:01 +0300

    Добавьте файлы проекта.

---
+++ SkyEx/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SkyEx.Models;
+
+namespace SkyEx.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }       
+    }
+}
