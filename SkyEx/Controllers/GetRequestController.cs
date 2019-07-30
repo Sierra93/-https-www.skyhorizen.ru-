c1230629a689daf9784ad1d5f881ef221ce5906a
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2019-07-30 23:47:18 +0300

    Полностью доделал почту

--- SkyEx/Controllers/GetRequestController.cs
+++ SkyEx/Controllers/GetRequestController.cs
@@ -10,7 +10,9 @@ using System.IO;
 using Microsoft.AspNetCore.Http;
 using System.Data;
 using System.Net;
-using System.Net.Mail;
+//using System.Net.Mail;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace SkyEx.Controllers {
     // Сохранение новой заявки в БД
@@ -19,13 +21,13 @@ namespace SkyEx.Controllers {
         [HttpPost]
         // GetRequestModel - Модель данных, с которой здесь работаем
         // Метод для работы с завками и сохраняем в БД
-        public JsonResult SaveRequest(GetRequestModel request) {
+        public async Task <JsonResult> SaveRequest(GetRequestModel request) {
             string connectionString = "Server=u499383.mssql.masterhost.ru,1433;Initial Catalog=u499383_skyexdb;Persist Security Info=False;User ID=u499383;Password=inessen9hoo;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=true;Connection Timeout=30";
             using (SqlConnection strConn = new SqlConnection(connectionString)) {
                 try {
                     var sClientName = request.sUserName;    // Имя клиента
                     var sEmailOrNumber = request.sEmailOrNumber;    // Почта или телефон клиента
-                    var sCommentResuest = request.sMultiTextRequest;    // Краткое описание проекта
+                    var sCommentResuest = request.sMultiTextRequest;    // Краткое описание проекта                    
                     // Проверяем все ли поля заполненны
                     if (string.IsNullOrWhiteSpace(sClientName) || (string.IsNullOrWhiteSpace(sEmailOrNumber))) {
                         sResult = "Error";
@@ -40,17 +42,18 @@ namespace SkyEx.Controllers {
                         using (var con = new SqlConnection(connectionString)) {
                             con.Open();
                             // Сохранение заявки в БД
-                            using (var com = new SqlCommand("INSERT INTO REQUEST(CLIENT_NAME, EMAIL_OR_NUMBER, COMMENT_REQUEST) VALUES('" + sClientName + "', '" + sEmailOrNumber + "', '" + sCommentResuest + "')", con)) {
+                            using (var com = new SqlCommand("INSERT INTO REQUEST(CLIENT_NAME, EMAIL_OR_NUMBER, COMMENT_REQUEST) VALUES('" + sClientName + "', '" + sEmailOrNumber + "', '" + sCommentResuest + "')", con)) {                                
                                 com.ExecuteNonQuery();
                                 sResult = "OK";
-                                SendMessage(request);
+                                await SendEmailAsync(request);
+                                //await SendEmailAsync(request);
                             }
                         }
                         //}   
                     }
                 }
                 catch (Exception ex) {
-                    new Exception(ex.Message.ToString());
+                    sResult = ex.Message.ToString();
                 }
                 finally {
                     if (strConn.State == ConnectionState.Open) {
@@ -60,21 +63,53 @@ namespace SkyEx.Controllers {
             }
             return Json(sResult);
         }
-        // Отправляем письма на почту
-        public JsonResult SendMessage(GetRequestModel request) {
-            SmtpClient client = new SmtpClient("smtp.mail.ru");
-            client.EnableSsl = true;
-            client.UseDefaultCredentials = false;
-            client.Credentials = new NetworkCredential("skyexx@mail.ru", "13467982dd");
-            MailMessage mailMessage = new MailMessage();
-            mailMessage.From = new MailAddress("skyexx@mail.ru");
-            mailMessage.To.Add("skyexx@mail.ru");
-            mailMessage.Body = "Имя: " + request.sUserName + "\n" +
+        public async Task SendEmailAsync(GetRequestModel request) {
+            try {
+                var emailMessage = new MimeMessage();
+                emailMessage.From.Add(new MailboxAddress("skyexx@mail.ru"));
+                emailMessage.To.Add(new MailboxAddress("skyexx@mail.ru"));
+                emailMessage.Subject = "Новая заявка";
+                emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) {
+                    Text = "Имя: " + request.sUserName + "\n" +
                 "E-mail или телефон: " + request.sEmailOrNumber + "\n" +
-                "Коротко о проекте: " + request.sMultiTextRequest;
-            mailMessage.Subject = "Новая заявка";
-            client.Send(mailMessage);            
-            return Json(request);
+                "Коротко о проекте: " + request.sMultiTextRequest
+            };
+                using (var client = new SmtpClient()) {
+                    await client.ConnectAsync("smtp.mail.ru", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                    await client.AuthenticateAsync("skyexx@mail.ru", "13467982dd");
+                    await client.SendAsync(emailMessage);
+                    await client.DisconnectAsync(true);
+                }
+            }
+            catch (Exception ex) {
+                new Exception(ex.Message.ToString());
+            }
         }
+        // Отправляем письма на почту
+        //public JsonResult SendMessage(GetRequestModel request) {
+        //    MailMessage message = new MailMessage();
+        //    message.To.Add(new MailAddress("skyexx@mail.ru")); // кому отправлять  
+        //    message.Body = "Имя: " + request.sUserName + "\n" +
+        //        "E-mail или телефон: " + request.sEmailOrNumber + "\n" +
+        //        "Коротко о проекте: " + request.sMultiTextRequest;
+        //    SmtpClient client = new SmtpClient("smtp.mail.ru");
+        //    client.Port = 587; // указываем порт 
+        //    client.Credentials = new NetworkCredential("skyhorizen@skyhorizen.ru", "13467982dd");
+
+        //    client.Send(message);  // отправить  
+        //    //SmtpClient client = new SmtpClient("mail.hosting.reg.ru");
+        //    //client.EnableSsl = true;
+        //    //client.UseDefaultCredentials = false;
+        //    //client.Credentials = new NetworkCredential("skyhorizen@skyhorizen.ru", "13467982dd");
+        //    //MailMessage mailMessage = new MailMessage();
+        //    //mailMessage.From = new MailAddress("skyhorizen@skyhorizen.ru");
+        //    //mailMessage.To.Add("skyexx@mail.ru");
+        //    //mailMessage.Body = "Имя: " + request.sUserName + "\n" +
+        //    //    "E-mail или телефон: " + request.sEmailOrNumber + "\n" +
+        //    //    "Коротко о проекте: " + request.sMultiTextRequest;
+        //    //mailMessage.Subject = "Новая заявка";
+        //    //client.Send(mailMessage);
+        //    return Json(request);
+        //}
     }
 }
