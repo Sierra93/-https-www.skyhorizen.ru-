1dd3dad44cc303036b31483fe55e6fd86eac2f68
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2019-12-01 23:32:34 +0300

    Редактиование контента

--- SkyEx/Controllers/AdminController.cs
+++ SkyEx/Controllers/AdminController.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace SkyEx.Controllers
-{
-    public class AdminController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace SkyEx.Controllers {
+    public class AdminController : Controller {
+        public IActionResult Index() {
             return View();
         }
     }
